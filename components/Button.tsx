f9491bab3693329bb24dd5299adaeeaa36a87513
limo-git <Dimpleyadav32@gmail.com>
2024-05-18 18:58:22 +0530

    commit

---
+++ components/Button.tsx
@@ -0,0 +1,10 @@
+"use client"
+import React from "react";
+
+export default function Button() {
+    return (
+        <>
+        <button className="border font-semibold border-white rounded-[30rem] w-40 h-12 hover:bg-white hover:text-black ">Wise Up</button>
+        </>
+    )
+}
